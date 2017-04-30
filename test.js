25a2bf1810af16dd2a8ebb7faaeca7e7228e6f1d
zhuqianye <q835041058@gmail.com>
2017-04-30 14:21:15 +0800

    first node

---
+++ test.js
@@ -0,0 +1,9 @@
+var http=require("http");
+
+http.createServer(function(req,res){
+	res.writeHead(200,{"Content-Type":"text/html"});
+	res.write("<h1>Node.js demo one</h1>");
+	res.end("<p>Hello World2</p>");
+}).listen(3000);
+
+console.log("http server is listening at port 3000");
