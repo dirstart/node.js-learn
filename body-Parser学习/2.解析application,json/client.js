d6704d54567de5c4fab23d50378fb0da62b7cd4e
dirstart <835041058@qq.com>
2018-06-12 22:05:40 +0800

    node body-parser解析application/json

---
+++ body-Parser学习/2.解析application,json/client.js
@@ -0,0 +1,26 @@
+const http = require('http');
+
+const options = {
+    hostname: '127.0.0.1',
+    port: '3000',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Content-Encoding': 'identify'
+    }
+};
+
+const reqJson = {
+    name: 'test'
+};
+
+const client = http.request(options, (res) => {
+    // todo
+    res.on('data', chunks => console.log(chunks.toString()));
+});
+
+client.on('error', error => console.log(error.message));
+
+const data = JSON.stringify(reqJson);
+
+client.end(data);
